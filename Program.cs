a88a3d81a4e605f23bca2146e3985280e4f3fa8f
LuisRoca11 <lroca1102@gmail.com>
2025-09-09 15:47:20 -0300

    numeros menores a 10

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10, 
+// imprimir en pantalla la leyenda "Todos los números son menores a diez".
+
+using System;
+
+namespace tres_num_menores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese numero 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese numero 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese numero 3: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1<10 && num2<10 && num3<10)
+            {
+                Console.WriteLine("Todos los números son menores a diez");
+            }
+            Console.ReadKey();
+        }
+    }
+}
